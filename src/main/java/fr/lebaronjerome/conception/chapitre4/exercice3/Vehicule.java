185d39ce5cfcbcc298df75f98f9c93548dbf109d
jerdopler <lebaronjerome@free.fr>
2016-02-27 15:16:42 +0100

    Premier commit

---
+++ src/main/java/fr/lebaronjerome/conception/chapitre4/exercice3/Vehicule.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package fr.lebaronjerome.conception.chapitre4.exercice3;
+
+/**
+ * @author Jerome
+ *
+ */
+public abstract class Vehicule {
+
+	private double vitesse = 0;
+
+	private double vitesseMax = 0;
+
+	private double longueur = 0;
+
+	/**
+	 * @param paramVitesse
+	 * @param paramVitesseMax
+	 */
+	public Vehicule(double paramLongueur, double paramVitesseMax) {
+		super();
+		longueur = paramLongueur;
+		vitesseMax = paramVitesseMax;
+	}
+
+	public void accelerer(double paramDeltaV) {
+
+	}
+
+	public void decelerer(double paramDeltaV) {
+
+	}
+
+	/**
+	 * @return the vitesse
+	 */
+	public double getVitesse() {
+		return vitesse;
+	}
+
+	/**
+	 * @return the longueur
+	 */
+	public double getLongueur() {
+		return longueur;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Vehicule [vitesse=" + vitesse + ", vitesseMax=" + vitesseMax + ", longueur=" + longueur + "]";
+	}
+
+}
